a1494864f4128688fa6418a94fc968dbe91f0d9c
Steve Rich <steven.john.rich@gmail.com>
2023-12-07 10:59:15 +0000

    Creating tests

--- rps.py
+++ rps.py
@@ -1,2 +1,3 @@
 def rock_paper_scissors(player1, player2):
+  return "player1"
   pass # replace with your program logic
--- rps_test.py
+++ rps_test.py
@@ -1,5 +1,30 @@
-import rps
+from rps import rock_paper_scissors
 
-def function_declared():
-  self.assertTrue( 'rock_paper_scissors' in globals() )
+# pip install pytest in the terminal
+# pytest -v rps_test.py
+
+def test_1_draw():
+    assert( rock_paper_scissors( 'paper', 'paper' ) == "draw" )
+    assert( rock_paper_scissors( 'rock', 'rock' ) == "draw" )
+    assert( rock_paper_scissors( 'scissors', 'scissors' ) == "draw" )
+
+
+def test_2_player1wins():
+    assert( rock_paper_scissors( 'scissors', 'paper' ) == "player1" )
+    assert( rock_paper_scissors( 'paper', 'rock' ) == "player1" )
+    assert( rock_paper_scissors( 'rock', 'scissors' ) == "player1" )
+
+
+def test_3_player2wins():
+    assert( rock_paper_scissors( 'rock', 'paper' ) == "player2" )
+    assert( rock_paper_scissors( 'scissors', 'rock' ) == "player2" )
+    assert( rock_paper_scissors( 'paper', 'scissors' ) == "player2" )
+
+
+def test_4_RPSLS_Draw():
+    assert( rock_paper_scissors( 'lizard', 'lizard' ) == "draw" )
+    assert( rock_paper_scissors( 'spock', 'spock' ) == "draw" )
+    
+
+
 
